2ba37bd97086d981e7c476dc2ff4b6887b48f0ff
Johan Hubau <johan.hubau@gmail.com>
2019-05-03 15:49:14 +0200

    Remove backend files
    See https://github.com/Johanhubau/Centscal-API for the backend.

--- exemple_davical_conf/etc_davical/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-//  $c->domain_name = "mydavicalsite.dyndns.org";
-//  $c->sysabbr     = 'rscds';
-  $c->admin_email = 'admin@example.net';
-  $c->system_name = "Really Simple CalDAV Store";
-//  $c->collections_always_exist = true;
-//  $c->enable_row_linking = true;
-  $c->default_locale = en_US.UTF-8;
-  $c->pg_connect[] = 'dbname=davical port=5432 user=davical_app';
